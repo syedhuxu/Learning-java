d7d56b9d391b57bad47c8f523fd43bafe3213cff
Syed Huzaif <syedhuzzu0198@gmail.com>
2026-09-04 18:40:31 +0530

    Simple Triangle

---
+++ src/SiimpleTriangle.java
@@ -0,0 +1,14 @@
+public class SiimpleTriangle {
+    public static  void main(String[] args){
+        System.out.println("hello world");
+        int n = 4;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
